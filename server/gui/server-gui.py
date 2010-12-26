6da500524ea862737addddff5077cdece160f08d
Yassen Bantchev <yassenb@gmail.com>
2010-12-26 15:46:47 +0200

    remove demo

--- server/gui/server-gui.py
+++ server/gui/server-gui.py
@@ -3,54 +3,74 @@
 
 import wx
 import socket
+import httplib
+import urllib
 import ConfigParser
+import os
+import re
 
 class IPValidator(wx.PyValidator):
-    def __init__(self):
+    def __init__(self, allow_hostname):
         wx.PyValidator.__init__(self)
+        self.__allow_hostname = allow_hostname
 
     def Clone(self):
-        return IPValidator()
+        return IPValidator(self.__allow_hostname)
 
     def Validate(self):
         text_ctrl = self.GetWindow()
 
         try:
-            socket.inet_aton(text_ctrl.GetValue())
-            text_ctrl.SetBackgroundColour(wx.SystemSettings_GetColour(wx.SYS_COLOUR_WINDOW))
-            text_ctrl.Refresh()
+            address = text_ctrl.GetValue()
+            socket.inet_aton(address)
         except socket.error:
-            wx.MessageBox("Please enter a valid IP adddress", "Error")
-            text_ctrl.SetBackgroundColour("pink")
-            text_ctrl.SetFocus()
-            text_ctrl.Refresh()
-            return False
+            if not (self.__allow_hostname and IPValidator.__is_valid_hostname(address)):
+                error_msg = "Please enter a valid IP adddress"
+                if self.__allow_hostname:
+                    error_msg += " or hostname"
+                wx.MessageBox(error_msg, "Error")
 
+                text_ctrl.SetBackgroundColour("pink")
+                text_ctrl.SetFocus()
+                text_ctrl.Refresh()
+                return False
+
+        text_ctrl.SetBackgroundColour(wx.SystemSettings_GetColour(wx.SYS_COLOUR_WINDOW))
+        text_ctrl.Refresh()
         return True
+
+    # copied from stackoverflow.com
+    @staticmethod
+    def __is_valid_hostname(hostname):
+        if len(hostname) > 255:
+            return False
+        if hostname[-1:] == ".":
+            hostname = hostname[:-1] # strip exactly one dot from the right, if present
+        allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+        return all(allowed.match(x) for x in hostname.split("."))
 
 class MainFrame(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self, None, title="DCS")
 
-        self.Bind(wx.EVT_CLOSE, self.__save_config)
+        self.__base_path = os.path.dirname(__file__) + "/"
+        master_ip, bind_ip, self.__registered = self.__read_config()
 
-        self.__read_config()
+        self.Bind(wx.EVT_CLOSE, self.__save_config)
 
         self.__tbicon = wx.TaskBarIcon()
         self.__tbicon.SetIcon(wx.Icon(self.__get_icon_name(), wx.BITMAP_TYPE_PNG), self.__get_tooltip())
         self.__tbicon.Bind(wx.EVT_TASKBAR_LEFT_DCLICK, self.__on_tbicon_click)
-        self.__hidden = False
 
-        self.Show(True)
+        self.Show(not self.__registered)
 
         panel = wx.Panel(self)
 
-        master_ip_static_text = wx.StaticText(panel, label="Master Node IP address:")
-        self.__master_ip_text_ctrl = wx.TextCtrl(panel, size=(120, 25), validator=IPValidator())
+        master_ip_static_text = wx.StaticText(panel, label="Master Node IP address or hostname:")
+        self.__master_ip_text_ctrl = wx.TextCtrl(panel, size=(250, 25), validator=IPValidator(True))
         bind_ip_static_text = wx.StaticText(panel, label="IP address to bind to (optional):")
-        self.__bind_ip_text_ctrl = wx.TextCtrl(panel, size=(120, 25), validator=IPValidator())
+        self.__bind_ip_text_ctrl = wx.TextCtrl(panel, size=(120, 25), validator=IPValidator(False))
 
-        master_ip, bind_ip = self.__read_config()
         self.__master_ip_text_ctrl.SetValue(master_ip or "")
         self.__bind_ip_text_ctrl.SetValue(bind_ip or "")
 
@@ -78,11 +98,32 @@ class MainFrame(wx.Frame):
         sizer.Fit(self)
 
     def __on_register(self, event):
-        self.__master_ip_text_ctrl.GetValidator().Validate()
-        if self.__bind_ip_text_ctrl.GetValue() != "":
-            self.__bind_ip_text_ctrl.GetValidator().Validate()
+        self.__registered = False
+        if not self.__master_ip_text_ctrl.GetValidator().Validate() or \
+           (self.__bind_ip_text_ctrl.GetValue() != "" and not self.__bind_ip_text_ctrl.GetValidator().Validate()):
+               return
+        if not self.__act_on_client({"action": "register"}):
+            wx.MessageBox("Problem registering with given Master Node", "Error")
+        else:
+            self.__registered = True
+            wx.MessageBox("Successfully registered with Master Node")
 
-        # TODO register
+    def __act_on_client(self, params):
+        master_ip = self.__master_ip_text_ctrl.GetValue()
+        bind_ip = self.__bind_ip_text_ctrl.GetValue()
+        if bind_ip:
+            conn = httplib.HTTPConnection(master_ip, source_address=bind_ip)
+        else:
+            conn = httplib.HTTPConnection(master_ip)
+
+        params = urllib.urlencode(params)
+        try:
+            conn.request("GET", "/cgi-bin/action.py?" + params)
+            response = conn.getresponse()
+            conn.close()
+            return response.status == 200
+        except:
+            return False
 
     def __get_icon_name(self):
         suffix = ""
@@ -90,7 +131,7 @@ class MainFrame(wx.Frame):
             suffix = "-16x16"
         elif "wxGTK" in wx.PlatformInfo:
             suffix = "-22x22"
-        return "network-idle" + suffix + ".png"
+        return self.__base_path + "icons/" + "network-idle" + suffix + ".png"
 
     def __get_tooltip(self):
         return "not connected"
@@ -99,30 +140,32 @@ class MainFrame(wx.Frame):
     __IPS_SECTION = "IPs"
     __MASTER_IP = "Master Node IP"
     __BIND_IP = "IP to bind to"
+    __REGISTERED = "Successfully registered"
     def __read_config(self):
         config = ConfigParser.SafeConfigParser()
         try:
-            config.read(self.__CONFIG_FILE_NAME)
+            config.read(self.__base_path + self.__CONFIG_FILE_NAME)
             master_ip = config.get(self.__IPS_SECTION, self.__MASTER_IP)
             bind_ip = config.get(self.__IPS_SECTION, self.__BIND_IP)
-            return (master_ip, bind_ip)
+            registered = config.getboolean(self.__IPS_SECTION, self.__REGISTERED)
+            return (master_ip, bind_ip, registered)
         except ConfigParser.Error:
-            return (None, None)
+            return (None, None, None)
 
     def __save_config(self, event):
         config = ConfigParser.SafeConfigParser()
         config.add_section(self.__IPS_SECTION)
         config.set(self.__IPS_SECTION, self.__MASTER_IP, self.__master_ip_text_ctrl.GetValue())
         config.set(self.__IPS_SECTION, self.__BIND_IP, self.__bind_ip_text_ctrl.GetValue())
+        config.set(self.__IPS_SECTION, self.__REGISTERED, str(self.__registered))
 
-        with open(self.__CONFIG_FILE_NAME, "wb") as configfile:
+        with open(self.__base_path + self.__CONFIG_FILE_NAME, "wb") as configfile:
             config.write(configfile)
 
         event.Skip()
 
     def __on_tbicon_click(self, event):
-        self.Show(self.__hidden)
-        self.__hidden = not self.__hidden
+        self.Show(not self.IsShown())
 
 app = wx.App()
 MainFrame()
