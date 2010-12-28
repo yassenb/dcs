97127b1047bf8353f7418463d0929f8d8ccceea2
Yassen Bantchev <yassenb@gmail.com>
2010-12-28 05:55:16 +0200

    set up junit infrastructure

---
+++ server/test/dcs/service/ComputingServiceTest.java
@@ -0,0 +1,11 @@
+package dcs.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ComputingServiceTest {
+    @Test public void passingTest() {
+        assertTrue(true);
+    }
+}
