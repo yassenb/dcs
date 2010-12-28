a40039b41c3c4e3cbf7a2b557b848072026fe7e9
Yassen Bantchev <yassenb@gmail.com>
2010-12-28 03:55:57 +0200

    reorganize build infrastructure

--- server/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-rmiregistry &
-java -Djava.rmi.server.hostname=192.168.1.3 -Djava.rmi.server.codebase=file:`pwd`/lib/executor.jar -Djava.security.policy=server.policy -jar lib/service.jar
