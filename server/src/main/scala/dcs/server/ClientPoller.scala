a54b1cb514ede4526e33f72daff7fe73f5bb9fd3
Yassen Bantchev <yassenb@gmail.com>
2011-08-05 15:36:55 +0300

    fix a bug with interrupts being sent when not sleeping rewrote the whole thing using actors, much cleaner now

--- server/src/main/scala/dcs/server/ClientPoller.scala
+++ server/src/main/scala/dcs/server/ClientPoller.scala
@@ -1,8 +1,10 @@
 package dcs.server
 
 import java.io.{OutputStream, InputStream}
-import dcs.common.{PingProtocol, TaskResponseProtocol, TaskRequestProtocol}
 import java.util.concurrent.TimeUnit
+import dcs.common.{PingProtocol, TaskResponseProtocol, TaskRequestProtocol}
+import actors.Actor
+import scala.concurrent.ops._
 
 class ClientPoller(createPingProtocol: (InputStream, OutputStream) => PingProtocol
                      = new PingProtocol(_, _),
@@ -13,31 +15,51 @@ class ClientPoller(createPingProtocol: (InputStream, OutputStream) => PingProtoc
                    executeTask: Array[Byte] => java.io.Serializable
                      = TaskExecutor.execute(_, new NetworkClassLoader),
                    executor: InterruptibleExecutor = new InterruptibleExecutor) {
-  def poll() {
-    SocketContext { (is, os) =>
-      createPingProtocol(is, os).requestTimeTillNextPing match {
-        case 0 => executeRemoteTask(Thread.currentThread())
-        case s => {
-          try {
-            TimeUnit.SECONDS.sleep(s)
-          } catch {
-            case ie: InterruptedException => // do nothing, this must be caused by a completed task
+  def startPolling() {
+    class Poller extends Actor {
+      def act() {
+        case object Wake
+        val wake = () => this ! Wake
+
+        var shouldBeSleeping = false
+        while (!shouldBeSleeping) {
+          SocketContext { (is, os) =>
+            createPingProtocol(is, os).requestTimeTillNextPing match {
+              case 0 => executeRemoteTask(wake)
+              case s => {
+                shouldBeSleeping = true
+                spawn {
+                  TimeUnit.SECONDS.sleep(s)
+                  wake()
+                }
+              }
+            }
           }
         }
+
+        receive {
+          case Wake =>
+        }
+
+        // spawning a new actor so that any leftover wake up messages get discarded
+        // this could maybe be a message to another actor and leave the job of spawning to it
+        (new Poller).start()
       }
     }
+    
+    (new Poller).start()
   }
 
-  private def executeRemoteTask(t: Thread) {
+  private def executeRemoteTask(signalFinish: () => Unit) {
     SocketContext { (is, os) =>
       val (taskID, objectBytes) = createTaskRequestProtocol(is, os).requestTask()
       
-      executor.submit({
+      executor.submit({() =>
         val answer = executeTask(objectBytes)
         SocketContext { (is, os) =>
           createTaskResponseProtocol(is, os).sendAnswer(taskID, answer)
         }
-        t.interrupt()
+        signalFinish()
       })
     }
   }
@@ -46,8 +68,6 @@ class ClientPoller(createPingProtocol: (InputStream, OutputStream) => PingProtoc
 object ClientPoller {
   def main(args: Array[String]) {
     val cp = new ClientPoller()
-    while (true) {
-      cp.poll()
-    }
+    cp.startPolling()
   }
 }
