92899758050fdbdd791eb932e1fe911c96a84311
Yassen Bantchev <yassenb@gmail.com>
2011-08-10 18:55:20 +0300

    improve UI looks

--- server/src/main/scala/dcs/server/ServerFrame.scala
+++ server/src/main/scala/dcs/server/ServerFrame.scala
@@ -1,11 +1,13 @@
 package dcs.server
 
 import swing._
+import Swing._
 import event.ButtonClicked
 
 class ServerFrame(applicationState: ApplicationState = new ApplicationState)
     extends MainFrame with StateEventSubscriber {
-  private val state = new Label
+  // initialize the label with some text otherwise for some reason it doesn't take up space when doing the layout
+  private val state = new Label(" ")
 
   applicationState.subscribe(this)
   
@@ -13,22 +15,55 @@ class ServerFrame(applicationState: ApplicationState = new ApplicationState)
     title = "dcs server"
 
     val remoteAddress = new TextField(30)
-    val localAddress = new TextField(30)
+    val localAddress = new TextField(30) {
+      // using HTML in order to have line breaks
+      tooltip = "<html>This is the locally configured network address that the program <br/> should bind to when" +
+        "connecting to the remote address. <br/> Leave blank to automatically pick one.</html>"
+    }
     val addresses = applicationState.getAddresses
     remoteAddress.text = addresses._1
     localAddress.text = addresses._3
     val updateButton = new Button("update")
 
-    contents = new BoxPanel(Orientation.Vertical) {
-      contents.append(
-        new Label("state"),
-        state,
-        new Label("remote address"),
-        remoteAddress,
-        new Label("local address to bind to"),
-        localAddress,
-        updateButton
+    contents = new GridBagPanel {
+      border = EmptyBorder(5)
+      val constraints = new Constraints {
+        fill = GridBagPanel.Fill.Horizontal
+        gridwidth = 0
+      }
+      def etchedWithBorder = {
+        CompoundBorder(EtchedBorder, EmptyBorder(0, 4, 2, 3))
+      }
+      val components = List(
+        new BoxPanel(Orientation.Vertical) {
+          border = TitledBorder(etchedWithBorder, "connection configuration")
+          contents.append(
+            new Label("remote address"),
+            remoteAddress,
+            RigidBox((0, 7)),
+            new Label("bind address"),
+            localAddress,
+            RigidBox((0, 5)),
+            new BoxPanel(Orientation.Horizontal) {
+              contents.append(
+                HGlue,
+                updateButton
+              )
+            }
+          )
+          contents.foreach({_.xLayoutAlignment = java.awt.Component.LEFT_ALIGNMENT})
+        },
+        RigidBox((0, 5)),
+        new BoxPanel(Orientation.Vertical) {
+          border = TitledBorder(etchedWithBorder, "state")
+          contents.append(
+            state
+          )
+        }
       )
+      for (component <- components) {
+        add(component, constraints)
+      }
     }
 
     listenTo(updateButton)
