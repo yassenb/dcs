4fc239f92687157deccd32966131e1d7822155e0
Yassen Bantchev <yassenb@gmail.com>
2011-08-11 20:19:19 +0300

    add a function override that doesn't need to have its input in a tuple

--- server/src/main/scala/dcs/server/SocketContext.scala
+++ server/src/main/scala/dcs/server/SocketContext.scala
@@ -4,8 +4,7 @@ import java.io.{OutputStream, InputStream}
 import java.net.{InetAddress, Socket}
 
 object SocketContext {
-  def apply[T](tuple: (String, Int, String))(f: (InputStream, OutputStream) => T): T = {
-    val (remoteAddress, port, localAddress) = tuple
+  def apply[T](remoteAddress: String, port: Int, localAddress: String)(f: (InputStream, OutputStream) => T): T = {
     val socket = new Socket(InetAddress.getByName(remoteAddress),
                             port,
                             if (localAddress != null) InetAddress.getByName(localAddress) else null,
@@ -15,5 +14,9 @@ object SocketContext {
     } finally {
       socket.close()
     }
+  }
+
+  def apply[T](tup: (String, Int, String))(f: (InputStream, OutputStream) => T): T = {
+    (apply: (String, Int, String) => ((InputStream, OutputStream) => T) => T).tupled(tup)(f)
   }
 }
