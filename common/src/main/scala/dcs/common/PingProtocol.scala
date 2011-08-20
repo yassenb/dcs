cd698eae2fd4baeb6533b5209f3e90de329fbbad
Yassen Bantchev <yassenb@gmail.com>
2011-08-20 14:52:33 +0300

    remove wrong comment

--- common/src/main/scala/dcs/common/PingProtocol.scala
+++ common/src/main/scala/dcs/common/PingProtocol.scala
@@ -6,11 +6,6 @@ import java.util.UUID
 class PingProtocol(in: InputStream, override protected val out: OutputStream) extends ProtocolWithIdentification {
   // TODO think about rewriting request and response so as to avoid code duplication and enforce type safety
 
-  /**
-   * Return either a task that should be executed or a number of seconds to sleep before the next ping
-   *
-   * @return {@code Either[(taskID:Int, objectBytes:Array[Byte]), (seconds:Int)]}
-   */
   def requestTaskOrSleepTime(serverID: UUID): Either[RequestedTask, Int] = {
     initiateCommunication(serverID, PingProtocol.id)
     
